8a4226b43a3f6738699ae0c39f0d1b8eaedb4374
Rafael Alves Freitas da Silva <46302153+rafaelafsinfo@users.noreply.github.com>
2023-10-28 22:08:08 -0300

    update

--- backend/src/controllers/login/index.js
+++ backend/src/controllers/login/index.js
@@ -4,7 +4,7 @@ const SessionController = {
     async createSession(req,res){
         const {username} = req.body
         try{
-            const user = await User.findOne({ username})
+            const user = await User.findOne({username})
             return res.status(200).json(user)
         } catch(err){
             return res.status(400).json(err)
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,6 +4,10 @@ const Schema = new mongoose.Schema({
     username: {
         type: String,
         required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 })
 
