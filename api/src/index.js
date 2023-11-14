58c96724d01bc4f9548eacd53a1df1e23cf547bc
Rafael Alves Freitas da Silva <46302153+rafaelafsinfo@users.noreply.github.com>
2023-11-14 20:49:31 -0300

    update

--- api/src/index.js
+++ api/src/index.js
@@ -6,15 +6,16 @@ require('dotenv').config()
 
 const routes = require('./routes')
 
-const app = express()
+
+
+const app = express() 
 mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology: true,
     useNewUrlParser:true
 },console.log('connect to database'))
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) 
 app.use(routes)
-
 
 app.listen(process.env.PORT || 3000, () => console.log('server running'))
